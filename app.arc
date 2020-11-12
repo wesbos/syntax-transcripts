57a15f939154ead951be45c9e78e1d9cff64c0d9
Wes Bos <wesbos@gmail.com>
2020-11-12 14:23:59 -0500

    scheduled function

--- app.arc
+++ app.arc
@@ -2,3 +2,9 @@
 begin-app
 
 @static
+
+@scheduled
+fetch-transcripts rate(10 minute)
+
+@aws
+timeout 300
---
+++ src/scheduled/fetch-transcripts/index.js
@@ -0,0 +1,5 @@
+// learn more about scheduled functions here: https://arc.codes/primitives/scheduled
+exports.handler = async function scheduled (event) {
+  console.log(JSON.stringify(event, null, 2))
+  return
+}
